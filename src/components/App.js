15fae62684da3665e6ea8e82a1ee609e58c1ae1b
SyDRoX <sydoruk.kostya@gmail.com>
2018-11-19 18:03:19 +0200

    added more stuff

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
+import Loader from './Loader';
 
 class App extends React.Component {
-    state = {images: []};
+    state = {images: [], submitted: false};
 
     onSearchSubmit = async term => {
+        this.setState({submitted : true});
+
         const response = await unsplash.get('/search/photos', {
             params: {
                 query: term
@@ -15,14 +18,22 @@ class App extends React.Component {
             console.log(response.data.results);
         })*/;
 
-        this.setState({images: response.data.results});
+        this.setState({images: response.data.results, submitted : false});
     };
+
+    renderContent(){
+        if (this.state.images && !this.state.submitted){
+            return <ImageList images={this.state.images}/>
+        }
+
+        return <Loader message="Please accept location request."/>;
+    }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
-                <ImageList images={this.state.images}/>
+                {this.renderContent()}
             </div>
         );
     }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import ImageCard from './ImageCard';
+import './ImageList.css'
 
 const ImageList = (props) => {
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image}/>
+    });
+
     return (
-        <div>
-            image list
+        <div className="image-list">
+            {images}
         </div>
     );
 };
